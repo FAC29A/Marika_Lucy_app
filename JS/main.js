be7f1328001cd361854e8edfcd01c6ad4e9d6ee1
MarikaBBB <126022615+MarikaBBB@users.noreply.github.com>
2023-10-18 18:38:19 +0100

    implement weather icons

--- JS/main.js
+++ JS/main.js
@@ -1,33 +1,61 @@
-// Import the getHistoricalWeather function from weatherFunctions.js
-//const getHistoricalWeather = window.getHistoricalWeather;
+function displayWeather(data) {
+    const weatherInfo = document.getElementById('earthWeatherInfo');
+    console.log("Data received:", JSON.stringify(data, null, 2));
 
-// Set default values for city and date
-const cityInput = document.getElementById('earthCityInput');
-const dateInput = document.getElementById('earthDateInput');
-cityInput.value = 'London';
-dateInput.value = '2023-10-08';
+    // Check if necessary data exists
+    if (!data || !data.forecast || !data.forecast.forecastday || !data.forecast.forecastday[0] || !data.forecast.forecastday[0].day) {
+        weatherInfo.innerHTML = `<p>Error: Missing weather data.</p>`;
+        return;
+    }
 
-// Trigger the getHistoricalWeather function with default values when the page loads
-getHistoricalWeather(new Event('submit')); // Pass a synthetic event to mimic form submission
+    // Assign the forecast data for the first day to dayForecast
+    const dayForecast = data.forecast.forecastday[0].day;
 
-// Add an event listener for the form submission
-document.addEventListener('DOMContentLoaded', function () {
-    const weatherForm = document.getElementById('weather-search-form');
-    
-    weatherForm.addEventListener('submit', async function (event) {
-        event.preventDefault(); // Prevent the default form submission.
+    // Continue with the rest of your code
+    const { location } = data;
 
-        // Get the historical weather data using the imported getHistoricalWeather function
-        try {
-            await getHistoricalWeather(event);
-        } catch (error) {
-            console.error('Error:', error.message);
+    // Set default values and destructure
+    const {
+        temp_c = 'N/A',
+        mintemp_c = 'N/A',
+        maxtemp_c = 'N/A',
+        avgtemp_c = 'N/A', // Ground temperature
+    } = dayForecast;
 
-            // Display the user-friendly error message.
-            const weatherInfo = document.getElementById('earthWeatherInfo');
-            weatherInfo.innerHTML = `<p>Error: ${error.message}</p>`;
-        }
-    });
-});
+    const { sunrise = 'N/A', sunset = 'N/A' } = data.forecast.forecastday[0].astro;
+
+    const rawDate = data.forecast.forecastday[0].date;
+    const formattedDate = formatDateForDisplay(rawDate);
+
+    // Format dates and temperatures
+    const formattedMinTemp = `${mintemp_c}°C`;
+    const formattedMaxTemp = `${maxtemp_c}°C`;
+    const formattedAvgTemp = `${avgtemp_c}°C`;
+
+    // Format pressure_mb
+    let pressure_mb = 'N/A'; // Default value
+    if (!isNaN(dayForecast.pressure_mb)) {
+        pressure_mb = parseFloat(dayForecast.pressure_mb);
+    }
+    console.log("Pressure Value:", pressure_mb);
+
+    // Get the icon class based on the weather condition
+    const iconClass = getIconClass(dayForecast.condition.code);
+
+    // Set the icon class for the weather icon
+    const iconElement = document.querySelector(".icon");
+    iconElement.className = 'icon ' + iconClass;
+
+    // First, set the general structure (innerHTML) of the weatherInfo.
+    weatherInfo.innerHTML = `
+        <h3>Weather for <span id="earthLocationName">${location.name}, ${location.country}</span> - <span id="earthDate">${formattedDate}</span></h3>
+        <p>Air Temperature (Min): <span id="earthMinTemperature">${formattedMinTemp}</span></p>
+        <p>Air Temperature (Max): <span id="earthMaxTemperature">${formattedMaxTemp}</span></p>
+        <p>Ground Temperature (Avg): <span id="earthAvgGroundTemperature">${formattedAvgTemp}</span></p>
+        <p>Atmospheric Pressure: <span id="earthPressure">${pressure_mb}</span> mb</p>
+        <p>Sunrise: <span id="earthSunrise">${sunrise}</span></p>
+        <p>Sunset: <span id="earthSunset">${sunset}</span></p>
+    `;
+}
 
 
--- JS/weatherDisplay.js
+++ JS/weatherDisplay.js
@@ -1,41 +1,62 @@
 function displayWeather(data) {
     const weatherInfo = document.getElementById('earthWeatherInfo');
+    console.log("Data received:", JSON.stringify(data, null, 2));
 
-    // Extract and display the weather information.
+    // Check if necessary data exists
+    if (!data || !data.forecast || !data.forecast.forecastday || !data.forecast.forecastday[0] || !data.forecast.forecastday[0].day) {
+        weatherInfo.innerHTML = `<p>Error: Missing weather data.</p>`;
+        return;
+    }
+
     const { location, forecast } = data;
+
+    // Set default values and destructure
     const {
-        temp_c,
-        mintemp_c,
-        maxtemp_c,
-        pressure_mb,
-        avgtemp_c, // Ground temperature
+        temp_c = 'N/A',
+        mintemp_c = 'N/A',
+        maxtemp_c = 'N/A',
+        avgtemp_c = 'N/A', // Ground temperature
     } = forecast.forecastday[0].day;
 
-    // Extract sunrise and sunset times
-    const { sunrise, sunset } = forecast.forecastday[0].astro;
+    const { sunrise = 'N/A', sunset = 'N/A' } = forecast.forecastday[0].astro;
 
     const rawDate = forecast.forecastday[0].date;
     const formattedDate = formatDateForDisplay(rawDate);
-
-    // Extract the date
-    const { date } = forecast.forecastday[0];
-
-    // Format the pressure value to hPa.
-    const pressure_hPa = pressure_mb / 100;
 
     // Format dates and temperatures
     const formattedMinTemp = `${mintemp_c}°C`;
     const formattedMaxTemp = `${maxtemp_c}°C`;
     const formattedAvgTemp = `${avgtemp_c}°C`;
 
-    // Display the weather information
+    // Format pressure_mb
+    let pressure_mb = 'N/A'; // Default value
+    if (!isNaN(forecast.forecastday[0].day.pressure_mb)) {
+        pressure_mb = parseFloat(forecast.forecastday[0].day.pressure_mb);
+    }
+    console.log("Pressure Value:", pressure_mb);
+
+    // Get the condition code from the data
+    const conditionCode = forecast.forecastday[0].day.condition.code;
+
+    // Get all weather icon elements
+    const iconElements = document.querySelectorAll(".weather-icon img");
+
+    // Set the src attribute for each icon element
+    iconElements.forEach((iconElement) => {
+        const iconFileName = `${conditionCode}.png`;
+        iconElement.src = `icons/${iconFileName}`;
+    });
+
+    // Set the general structure (innerHTML) of the weatherInfo.
     weatherInfo.innerHTML = `
         <h3>Weather for <span id="earthLocationName">${location.name}, ${location.country}</span> - <span id="earthDate">${formattedDate}</span></h3>
-        <p>Air Temperature (Min): ${formattedMinTemp}</p>
-        <p>Air Temperature (Max): ${formattedMaxTemp}</p>
-        <p>Ground Temperature (Avg): ${formattedAvgTemp}</p>
-        <p>Atmospheric Pressure: ${pressure_hPa} hPa</p>
-        <p>Sunrise: ${sunrise}</p>
-        <p>Sunset: ${sunset}</p>
+        <p>Air Temperature (Min): <span id="earthMinTemperature">${formattedMinTemp}</span></p>
+        <p>Air Temperature (Max): <span id="earthMaxTemperature">${formattedMaxTemp}</span></p>
+        <p>Ground Temperature (Avg): <span id="earthAvgGroundTemperature">${formattedAvgTemp}</span></p>
+        <p>Atmospheric Pressure: <span id="earthPressure">${pressure_mb}</span> mb</p>
+        <p>Sunrise: <span id="earthSunrise">${sunrise}</span></p>
+        <p>Sunset: <span id="earthSunset">${sunset}</span></p>
     `;
 }
+
+    
